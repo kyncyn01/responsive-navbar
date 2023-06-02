4a6b0b124fe1ad0f69eb9b238bdbd3d54ffcdf94
kyncyn01 <135114921+kyncyn01@users.noreply.github.com>
2023-06-02 13:16:34 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-<meta name="application-name" content="PWA App" />
+<meta name="application-name" content="Roro Bot App" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-<meta name="apple-mobile-web-app-title" content="PWA App" />
-<meta name="description" content="Best PWA App in the world" />
+<meta name="apple-mobile-web-app-title" content="Roro Bot App" />
+<meta name="description" content="First Roro Bot Best PWA App in the world" />
 <meta name="format-detection" content="telephone=no" />
 <meta name="mobile-web-app-capable" content="yes" />
 <meta name="msapplication-config" content="/icons/browserconfig.xml" />
@@ -24,14 +24,14 @@
 
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:url" content="https://yourdomain.com" />
-<meta name="twitter:title" content="PWA App" />
-<meta name="twitter:description" content="Best PWA App in the world" />
+<meta name="twitter:title" content="Roro Bot App" />
+<meta name="twitter:description" content="Roro Bot Best PWA App in the world" />
 <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
-<meta name="twitter:creator" content="@DavidWShadow" />
+<meta name="twitter:creator" content="@KinYuen" />
 <meta property="og:type" content="website" />
-<meta property="og:title" content="PWA App" />
-<meta property="og:description" content="Best PWA App in the world" />
-<meta property="og:site_name" content="PWA App" />
+<meta property="og:title" content="Roro Bot App" />
+<meta property="og:description" content="Roro Bot Best PWA App in the world" />
+<meta property="og:site_name" content="Roro Bot App" />
 <meta property="og:url" content="https://yourdomain.com" />
 <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
 
