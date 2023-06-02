f4cd7e52b5cdd403920e0874ed3a7e6cc60f017c
kyncyn01 <135114921+kyncyn01@users.noreply.github.com>
2023-06-02 13:06:59 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+<meta
+  name='viewport'
+  content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+/>
 <meta name="application-name" content="Roro Bot App" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
